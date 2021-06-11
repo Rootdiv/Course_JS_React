e8e291d4e97302e0f4c196a6730cacaf0dd1e3ae
Rootdiv <rootdiv@mail.ru>
2021-06-11 11:06:24 +0300

    fix: lesson01  Es6

--- js/script.js
+++ js/script.js
@@ -4,10 +4,8 @@ const employers = ['АртеМ', 'максим', 'Владимир', 'серге
   ' ', 'виктория ', 'ЕкаТерина', '', ' Андрей ', 'КИРИЛЛ'
 ];
 const nameCourse = 'Базовый React';
-const command = [];
-
-command.push(employers.filter(item => item.trim() !== '')
-  .map(str => str.trim().charAt(0).toUpperCase() + str.trim().slice(1).toLowerCase()).join(','));
+const command = employers.filter(item => item.trim() !== '')
+  .map(str => str.trim().charAt(0).toUpperCase() + str.trim().slice(1).toLowerCase()).join(',');
 
 const data = {
   cash: [3, 8, 3],
@@ -15,21 +13,23 @@ const data = {
   add: ['styled-components', 'firebase']
 };
 
-const calcCash = (own) => {
-  const everyCash = own;
-  const total = everyCash.reduce((sum, current) => sum + current, 0);
-  return total;
-};
+const {
+  cash,
+  react,
+  react: [first],
+  add
+} = data;
 
-const lesson = calcCash(data.cash);
+const calcCash = everyCash => everyCash.reduce((sum, current) => sum + current, 0);
 
-const makeBusiness = (director, teacher, allModule, gang, course) => {
-  teacher = teacher || 'Максим';
-  const sumTech = [...data.react, ...data.add, 'и другие'];
+const lesson = calcCash(cash);
+
+const makeBusiness = (director, allModule, gang, course, teacher = 'Максим') => {
+  const sumTech = [...react, ...add, 'и другие'];
   console.log(`Стартуем новый курс: "${course}". Владелец: ${director}, преподаватель: ${teacher}. Всего уроков: ${allModule}.\nКоманда Академии: ${gang}`);
-  console.log(`Первое что изучим будет ${data.react[0]}. Он очень похож на HTML!`);
+  console.log(`Первое что изучим будет ${first}. Он очень похож на HTML!`);
   console.log('Технологии которые мы изучим: ');
   console.log(...sumTech);
 };
 
-makeBusiness(...['Артем', null, lesson, command, nameCourse]);
+makeBusiness('Артем', lesson, command, nameCourse);
