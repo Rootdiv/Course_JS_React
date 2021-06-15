f29983bbb843fb784fb16ce466c87d289851d1ff
Rootdiv <rootdiv@mail.ru>
2021-06-15 12:15:06 +0300

    fix: lesson03. Functional Component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createGlobalStyle } from 'styled-components';
 import { NavBar } from './Components/NavBar';
 
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,7 +1,7 @@
+import React from 'react';
 import styled from 'styled-components';
 import logoImg from '../image/logo.svg';
-import userImg from '../image/user.svg';
-
+import signImg from '../image/sign.svg';
 
 const NavBarStyled = styled.header`
   position: fixed;
@@ -11,7 +11,7 @@ const NavBarStyled = styled.header`
   width: 100%;
   display: flex;
   justify-content: space-between;
-  padding 15px;
+  padding: 15px;
   background-color: #299B01;
   color: #fff
 `;
@@ -30,21 +30,9 @@ const ImgLogo = styled.img`
   width: 50px;
 `;
 
-const Login = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const ImgUser = styled.img`
-  width: 32px;
-  margin-bottom: 5px;
-`;
-
-const Button = styled.button`
-  color: #fff;
-  padding: 0;
-  margin: 0;
+const Login = styled.button`
+  cursor: pointer;
+  color: #ffffff;
   border: none;
   background: transparent;
 `;
@@ -52,12 +40,12 @@ const Button = styled.button`
 export const NavBar = () => (
   <NavBarStyled>
     <Logo>
-      <ImgLogo src={logoImg} alt="logo" />
+      <ImgLogo src={logoImg} alt="лого" />
       <H1>MrDonald's</H1>
     </Logo>
     <Login>
-      <ImgUser src={userImg} alt="user" />
-      <Button>Войти</Button>
+      <img src={signImg} alt="войти" />
+      <p>Войти</p>
     </Login>
   </NavBarStyled>
 );
