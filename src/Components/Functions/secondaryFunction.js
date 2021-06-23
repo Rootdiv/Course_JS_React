e66498d3e5aa6d9508be52bb85fce1dbab1cb16e
Rootdiv <rootdiv@mail.ru>
2021-06-23 13:32:54 +0300

    fix: lesson09. React Hooks. Add-ons

--- src/Components/Functions/secondaryFunction.js
+++ src/Components/Functions/secondaryFunction.js
@@ -1,11 +1,7 @@
 export const totalPriceItems = order => {
-  const countTopping = order.topping && order.topping.filter(item => item.checked).length;
+  const countTopping = order.topping ? order.topping.filter(item => item.checked).length : 0;
   const priceTopping = (order.price * 0.1) * countTopping;
-  if (countTopping) {
-    return (order.price + priceTopping) * order.count;
-  } else {
-    return order.price * order.count;
-  }
+  return (order.price + priceTopping) * order.count;
 };
 
 export const formatCurrency = value => value.toLocaleString('ru-RU', {
--- src/Components/Order/OrderListItem.jsx
+++ src/Components/Order/OrderListItem.jsx
@@ -5,8 +5,8 @@ import { totalPriceItems, formatCurrency } from '../Functions/secondaryFunction'
 
 const OrderItemStyled = styled.li`
   display: flex;
-  flex-direction: column;
   margin: 15px 0;
+  flex-wrap: wrap;
 `;
 
 const ItemName = styled.span`
@@ -31,21 +31,21 @@ const TrashButton = styled.button`
   background-repeat: no-repeat;
 `;
 
-const ItemTopping = styled.div`
+const Toppings = styled.div`
   font-size: 14px;
   color: #9A9A9A;
+  width: 100%;
 `;
 
-export const OrderListItem = ({ order }) => (
-  <OrderItemStyled>
-    <div style={{ display: 'flex' }}>
+export const OrderListItem = ({ order }) => {
+  const toppings = order.topping && order.topping.filter(item => item.checked).map(item => item.name).join(', ');
+  return (
+    <OrderItemStyled>
       <ItemName>{order.name}</ItemName>
       <span>{order.count}</span>
       <ItemPrice>{formatCurrency(totalPriceItems(order))}</ItemPrice>
       <TrashButton />
-    </div>
-    <ItemTopping>
-      {order.topping.map(order => order.checked && <span>{order.name} </span>)}
-    </ItemTopping>
-  </OrderItemStyled>
-);
+      {toppings && <Toppings key={order.id}>Допы: {toppings}</Toppings>}
+    </OrderItemStyled>
+  );
+};
