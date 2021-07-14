bd37ae6d4dbbff3cb733b5fda6fb489eb94cb6e8
Rootdiv <rootdiv@mail.ru>
2021-07-14 15:22:05 +0300

    fix: Calculator

--- src/Components/Modal/CountItem.jsx
+++ src/Components/Modal/CountItem.jsx
@@ -23,9 +23,9 @@ export const CountItem = () => {
     <CountWrapper>
       <span>Количество</span>
       <div>
-        <ButtonCount disabled={count <= 1} onClick={() => setCount(count - 1)}>-</ButtonCount>
+        <ButtonCount disabled={count <= 1} onClick={() => setCount(+count - 1)}>-</ButtonCount>
         <CountInput type="number" min="1" max="100" value={count < 1 ? 1 : count} onChange={onChange} />
-        <ButtonCount onClick={() => setCount(count + 1)}>+</ButtonCount>
+        <ButtonCount onClick={() => setCount(+count + 1)}>+</ButtonCount>
       </div>
     </CountWrapper>
   );
